f106ad4b55b7ebfdd4dba6bd97f623f5da031188
Preslav Rachev <rachev.preslav@gmail.com>
2018-04-25 07:07:01 +0200

    Allow for end images to be saved with their original size

--- colorizer.py
+++ colorizer.py
@@ -10,13 +10,27 @@ from skimage.color import lab2rgb, rgb2lab
 
 @easyargs
 def main(src_image_path, dest_image_path):
-    src_image_xy = turn_image_into_input_and_output(src_image_path)
+    src_image_xy = turn_image_into_input_and_output(src_image_path, target_size=(256, 256))
     dest_image_xy = turn_image_into_input_and_output(dest_image_path)
     model = create_and_train_model(*src_image_xy)
     colorize_image(dest_image_xy[0], model)
 
-def turn_image_into_input_and_output(image_path, target_size=(200,200)):
-    image = img_to_array(load_img(image_path, target_size=target_size)) / 255
+def turn_image_into_input_and_output(image_path, target_size=None):
+    '''
+    Loads, resizes, and converts input images into inputs and outputs for the NN model
+    '''    
+    img = load_img(image_path, target_size=target_size)
+    width_offset = 0
+    height_offset = 0
+    if (img.size[0] % 2 != 0):
+        width_offset = 1
+    if (img.size[1] % 2 != 0):
+        height_offset = 1
+    
+    if width_offset != 0 or height_offset != 0:
+        img = img.resize((img.size[0] + width_offset, img.size[1] + height_offset))
+
+    image = img_to_array(img) / 255
     lab_image = rgb2lab(image)
     lab_image_norm = (lab_image + [0, 128, 128]) / [100, 255, 255]
 
@@ -57,7 +71,7 @@ def create_and_train_model(X, Y, epochs=100):
 
 def colorize_image(X, model, name='result.jpg'):
     output = model.predict(X)
-    cur = np.zeros((200, 200, 3))
+    cur = np.zeros((output.shape[1], output.shape[2], 3))
     cur[:,:,0] = X[0][:,:,0]
     cur[:,:,1:] = output[0]
 
